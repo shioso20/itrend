0030a0b98e3c352f9f63bd83b8c3f220f4a3f542
Tony Kosh <88734315+koshtony@users.noreply.github.com>
2022-05-16 13:54:17 +0300

    ----

--- market.py
+++ market.py
@@ -6,7 +6,7 @@ import plotly.express as px
 def visuals(data):
     col1=st.sidebar
     col2,col3=st.columns((1,1))
-    row=col1.selectbox("bar plot x-axis",data.columns)
+    row=col1.selectbox("Incoming Orders bar plot x-axis",data.columns)
     time_plot(data,"date","quant")
     try:
         bar(data,row,"quant")
@@ -16,7 +16,24 @@ def bar(data,row,col):
     fig=plt.figure(figsize=(8,3))
     sn.barplot(x=row,y=col,data=data,estimator=sum)
     st.pyplot(fig)
+def counts(data,row):
+    fig=plt.figure(figsize=(8,3))
+    sn.countplot(x=row,data=data)
+    st.pyplot(fig)
+
 def time_plot(df,x,y):
     fig = px.line(df, x=x, y=y, title='sales over time')
     fig.update_xaxes(rangeslider_visible=True)
     st.plotly_chart(fig)
+def dis_visuals(data):
+    col1=st.sidebar
+    col2,col3=st.columns((1,1))
+    row=col1.selectbox("Dispatch bar plot x-axis",data.columns)
+    try:
+        counts(data,row)
+    except:
+        st.warning("select appropriate field")
+
+
+
+        
